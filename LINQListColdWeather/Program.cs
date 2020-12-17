cd386280d8c3e8711b4521ef36252552b544ae7a
Hinkle <22HinkleK@mydacc.org>
2020-12-17 09:34:52 -0500

    First commit

---
+++ LINQListColdWeather/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQListColdWeather
+{
+
+    public enum DayOfWeek
+    {
+        Sun, Mon, Tues, Wed, Thurs, Fri, Sat
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Weather> daysForcasted = new List<Weather>();
+            daysForcasted.Add(new Weather(DayOfWeek.Tues, 36, 28));
+            daysForcasted.Add(new Weather(DayOfWeek.Wed, 34, 29));
+            daysForcasted.Add(new Weather(DayOfWeek.Thurs, 36, 26));
+            daysForcasted.Add(new Weather(DayOfWeek.Fri, 37, 28));
+            daysForcasted.Add(new Weather(DayOfWeek.Sat, 42, 33));
+
+            var coldDays = from forecast in daysForcasted where forecast.HighTemp < 40 select forecast;
+
+            foreach(var coldDay in coldDays)
+            {
+                Console.WriteLine("Cold weather on {0}",coldDay.Day);
+            }
+        }
+    }
+}
---
+++ LINQListColdWeather/Weather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQListColdWeather
+{
+    public class Weather
+    {
+        public DayOfWeek Day;
+        public int HighTemp;
+        public int LowTemp;
+
+        public Weather(DayOfWeek day, int high, int low)
+        {
+            Day = day;
+            HighTemp = high;
+            LowTemp = low;
+        }
+    }
+}
